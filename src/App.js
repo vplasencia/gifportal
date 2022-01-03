c6f4b1ce61f52c47d5908e16006b746c3c606eff
vplasencia <v.pcalana@gmail.com>
2022-01-03 00:32:59 -0500

    Added gif grid and form

--- src/App.js
+++ src/App.js
@@ -6,6 +6,14 @@ import twitterLogo from "./assets/twitter-logo.svg";
 const TWITTER_HANDLE = "ViviPlasenciaC";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
+const TEST_GIFS = [
+  "https://media.giphy.com/media/SGkufeMafyuBhIw796/giphy.gif",
+  "https://media.giphy.com/media/a42CE9jJY8zYGU51bG/giphy.gif",
+  "https://media.giphy.com/media/iK636zveaFVRR0SRFu/giphy.gif",
+  "https://media.giphy.com/media/tHvMDELcZVSpBDzDRr/giphy.gif",
+  "https://media.giphy.com/media/FWcoE5AkG3BRe/giphy.gif"
+];
+
 function App() {
   /*
    * This function holds the logic for deciding if a Phantom Wallet is
@@ -33,6 +41,9 @@ function App() {
     }
   };
   const [walletAddress, setWalletAddress] = useState(null);
+  const [inputValue, setInputValue] = useState("");
+  const [gifList, setGifList] = useState([]);
+
   const connectWallet = async () => {
     const { solana } = window;
 
@@ -42,13 +53,52 @@ function App() {
       setWalletAddress(response.publicKey.toString());
     }
   };
+  const sendGif = async () => {
+    if (inputValue.length > 0) {
+      console.log("Gif link:", inputValue);
+      setGifList([...gifList, inputValue]);
+      setInputValue("");
+    } else {
+      console.log("Empty input. Try again.");
+    }
+  };
+  const onInputChange = (event) => {
+    const { value } = event.target;
+    setInputValue(value);
+  };
   const renderNotConnectedContainer = () => (
-    <button
-      className="cta-button connect-wallet-button"
-      onClick={connectWallet}
-    >
+    <button className="connect-wallet-button" onClick={connectWallet}>
       Connect to Wallet
     </button>
+  );
+  const renderConnectedContainer = () => (
+    <div className="connected-container">
+      <form
+        className="gif-form"
+        onSubmit={(event) => {
+          event.preventDefault();
+          sendGif();
+        }}
+      >
+        <input
+          autoFocus
+          type="text"
+          placeholder="Enter gif link!"
+          value={inputValue}
+          onChange={onInputChange}
+        />
+        <button type="submit" className="submit-gif-button">
+          Add Gif
+        </button>
+      </form>
+      <div className="gif-grid">
+        {gifList.map((gif, i) => (
+          <div className="gif-item" key={i}>
+            <img src={gif} alt={gif} />
+          </div>
+        ))}
+      </div>
+    </div>
   );
   useEffect(() => {
     const onLoad = async () => {
@@ -57,20 +107,35 @@ function App() {
     window.addEventListener("load", onLoad);
     return () => window.removeEventListener("load", onLoad);
   }, []);
+  useEffect(() => {
+    if (walletAddress) {
+      console.log("Fetching GIF list...");
+
+      // Call Solana program here.
+
+      // Set state
+      setGifList(TEST_GIFS);
+    }
+  }, [walletAddress]);
   return (
-    <div className="flex flex-col min-h-screen px-2 bg-gray-900">
-      <div className="flex items-center justify-center absolute inset-0 px-2">
+    <div className="app">
+      <div className="flex items-center justify-center mt-20">
         <div className="text-center space-y-5">
-          <p className="font-medium text-3xl text-white">🖼 GIF Portal</p>
-          <p className="text-white text-xl">
-            View your GIF collection in the metaverse ✨
-          </p>
+          <div className="mb-20">
+            <p className="font-medium text-3xl text-white mb-5">
+              🖼 Dance GIF Portal
+            </p>
+            <p className="text-white text-xl">
+              View your Dance GIF collection in the metaverse ✨
+            </p>
+          </div>
           {!walletAddress && renderNotConnectedContainer()}
+          {walletAddress && renderConnectedContainer()}
         </div>
       </div>
-      <footer className="absolute inset-x-0 bottom-0 h-16">
+      <footer className="footer">
         <a
-          className="flex items-center justify-center text-white underline hover:no-underline"
+          className="flex items-center justify-center text-white underline hover:no-underline w-fit"
           href={TWITTER_LINK}
           target="_blank"
           rel="noreferrer noopener nofollow"
